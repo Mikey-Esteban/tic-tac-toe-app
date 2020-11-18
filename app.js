e8a687c506833197d51aecdffe2964307c5531a1
Mikey Esteban <michaelp.esteban@gmail.com>
2020-11-18 10:34:16 -0500

    created gameboard module, gameplay module, UI iife

--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+// Module for Gameboard
+const Gameboard = () => {
+  let board =
+  [
+    [
+      null, null, null
+    ],
+    [
+      null, null, null
+    ],
+    [
+      null, null, null
+    ]
+  ]
+
+  const placePiece = (type, id) => {
+    const legend = {
+      'one': { row: 0, col: 0 },
+      'two': { row: 0, col: 1 },
+      'three': { row: 0, col: 2 },
+      'four': { row: 1, col: 0 },
+      'five': { row: 1, col: 1 },
+      'six': { row: 1, col: 2 },
+      'seven': { row: 2, col: 0 },
+      'eight': { row: 2, col: 1 },
+      'nine': { row: 2, col: 2 }
+    }
+    const { row, col } = legend[id];
+    console.log(`row is ${row}`);
+    console.log(`col is ${col}`);
+    board[row][col] = type;
+  }
+
+  const isFull = () => {
+    for (let arr of board) {
+      if (!arr[0] || !arr[1] || !arr[2]) {
+        return false
+      }
+    }
+    return true
+  }
+
+  const isWon = () => {
+    if (
+      // Check each Column
+      board[0][0] && board[0][0] == board[1][0] && board[0][0] == board[2][0] ||
+      board[0][1] && board[0][1] == board[1][1] && board[0][1] == board[2][1] ||
+      board[0][2] && board[0][2] == board[1][2] && board[0][2] == board[2][2] ||
+      // Check each Row
+      board[0][0] && board[0][0] == board[0][1] && board[0][0] == board[0][2] ||
+      board[1][0] && board[1][0] == board[1][1] && board[1][0] == board[1][2] ||
+      board[2][0] && board[2][0] == board[2][1] && board[2][0] == board[2][2] ||
+      // Check Diagonals
+      board[0][0] && board[0][0] == board[1][1] && board[0][0] == board[2][2] ||
+      board[0][2] && board[0][2] == board[1][1] && board[0][2] == board[2][0]
+    ) { return true }
+
+    return false
+
+  }
+
+  return { board, placePiece, isFull, isWon }
+}
+
+// Module for Gameplay
+const Gameplay = () => {
+  let player = 'X';
+
+  const switchPlayer = () => {
+    return player == 'X' ? player = 'O' : player = 'X';
+  }
+
+  const board = Gameboard();
+
+  return { player, switchPlayer, board }
+};
+
 // IIFE for User Interface
 const UI = (() => {
   const box1 = document.querySelector('#one');
@@ -14,10 +91,17 @@ const UI = (() => {
 
   const allPositions = [ box1, box2, box3, box4, box5, box6, box7, box8, box9 ];
 
+  game = Gameplay();
+
   for (let box of allPositions) {
     box.addEventListener('click', () => {
-      console.log(`i ran!`);
-      box.innerText = 'X';
+      // change UI box
+      box.innerText = game.player;
+      // add piece to gameboard
+      game.board.placePiece(game.player, box.id)
+      // switch player
+      game.player = game.switchPlayer();
+
       turnDescription.innerText == 'Player X turn' ?
         turnDescription.innerText = 'Player O turn' :
         turnDescription.innerText = 'Player X turn' ;
